9de64575c8e2532d951961be779d6de824200a4e
marcosasesores <marcos.sanchez@asesores.es>
2024-06-27 11:25:38 +0200

    cambios realizado

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express')
-const app = express()
-const port = 8080
+var hbs = require('hbs');
+
+const app = express();
+const port = 8080;
+
+
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Servir contenido estático
 app.use(express.static('public'));
 
 
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Fernando herrera',
+        titulo: 'curso node'
+    });
+});
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname + '/public/generic.html');
 });
